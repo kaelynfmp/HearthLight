4be2fc163dbd8c3050563bfa9321c10535c413e2
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-02-04 02:40:45 -0700

    feat: adjust defaults and add comments for items

--- scripts/resources/item.gd
+++ scripts/resources/item.gd
@@ -4,18 +4,23 @@ class_name Item
 
 extends Resource
 
+## Icon that represents the sprite in-game.
 @export var sprite: Texture2D
+## Name of the item as it appears in-game.
 @export var name: String
-@export_multiline var desc: String
-@export var maxStack: int
-@export var price: int
+## In-game description of item.
+@export_multiline var description: String
 
-# Make sure that every parameter has a default value.
-# Otherwise, there will be problems with creating and editing
-# your resource via the inspector.
-func _init(p_sprite = null, p_name = "", p_desc = "", p_maxStack = 0, p_price = 0):
+@export_category("Properties")
+## Maximum amount of items that can be stored in one inventory slot.
+@export_range(1, 999) var maxStack: int
+## Price of an item if you were to sell it as junk. Purchase prices are determined algorithmically based off of this value.
+@export_range(0, 999999999999999999) var price: int
+
+## Initialization of the default values
+func _init(p_sprite = null, p_name = "", p_desc = "", p_maxStack = 999, p_price = 0):
 	sprite = p_sprite
 	name = p_name
-	desc = p_desc
+	description = p_desc
 	maxStack = p_maxStack
 	price = p_price
