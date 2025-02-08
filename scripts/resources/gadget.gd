bcf9dbfed96595a56fff939bc8267ce6cef11784
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-02-07 21:16:16 -0700

    feat: gadgets backend

---
+++ scripts/resources/gadget.gd
@@ -0,0 +1,34 @@
+@icon("res://resources/resource-icons/gadget.svg")
+
+class_name Gadget
+
+extends Resource
+
+## Texture that represents the sprite in-game.
+@export var texture: Texture2D
+## Name of the gadget as it appears in-game.
+@export var name: String
+## In-game description of the gadget.
+@export_multiline var description: String
+
+@export_category("Properties")
+## Item corresponding ot the gadget for inventory usage
+@export var item: Item
+## Inventory that belongs to the gadget
+@export var inventory: Inventory
+## Whether or not this gadget can have recipes
+@export var produces: bool
+## The amount of inputs a recipe from this gadget can take
+@export var inputs: int
+## The amount of outputs this gadget can produce
+@export var outputs: int
+
+func _init(p_texture: Texture2D = null, p_name: String = "", p_description: String = "", p_item: Item = null, p_inventory: Inventory = null, p_produces: bool = false, p_inputs: int = 0, p_outputs: int = 0):
+	texture = p_texture
+	name = p_name
+	description = p_description
+	item = p_item
+	inventory = p_inventory
+	produces = p_produces
+	inputs = p_inputs
+	outputs = p_outputs
