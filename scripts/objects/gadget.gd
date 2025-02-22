e04e7106391d666f0462f697748032c6aa238ab0
Duc Ho <hotienduc05012002@gmail.com>
2025-02-22 13:22:06 -0700

    Added mouse hold event that only activate when player nearby

--- scripts/objects/gadget.gd
+++ scripts/objects/gadget.gd
@@ -4,20 +4,31 @@ extends StaticBody2D
 
 @export var output_inventory: Inventory
 
+@export var is_holding: bool = false
+
 var character: Node2D
+
+var base_layer: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	character = get_parent().get_parent().get_parent().find_child("Character")
+	base_layer = get_parent()
 	$Sprite2D.texture = gadget_stats.texture
 	$Timer.wait_time = gadget_stats.process_time
-	$Timer.start()
 	$Timer.timeout.connect(add_item_to_inventory)
-	$TextureProgressBar.visible = true
+	$TextureProgressBar.visible = false
+	$TextureProgressBar.value = 0
+	$Timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if is_holding:
+		$Timer.paused = false
+	else:
+		$Timer.paused = true
+		
 	$TextureProgressBar.value = 100 - ($Timer.time_left / $Timer.wait_time) * 100
 	
 func add_item_to_inventory() -> void:
@@ -27,3 +38,23 @@ func add_item_to_inventory() -> void:
 ## 'Collects' a given item, placing it into the inventory on the nearest open slot
 func collect(item: Item):
 	character.inventory.insert(item)
+
+# Temporary highlight
+func _on_mouse_entered() -> void:
+	$TextureProgressBar.visible = true
+	$Sprite2D.self_modulate = Color(1.0, 1.0, 1.0, 0.5)
+
+func _on_mouse_exited() -> void:
+	$TextureProgressBar.visible = false
+	$Sprite2D.self_modulate = Color(1.0, 1.0, 1.0, 1.0) # Replace with function body.
+
+func detect_nearby() -> bool:
+	var character_cell_position: Vector2 = base_layer.local_to_map(character.global_position)
+	var gadget_cell_position: Vector2 = base_layer.local_to_map(global_position)
+	return character_cell_position.distance_squared_to(gadget_cell_position) <= 2.0
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if detect_nearby() and event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed: # Replace with function body.
+		is_holding = true
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
+		is_holding = false
