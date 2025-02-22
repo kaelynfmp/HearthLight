a81ebf02d93f6ac042819166020f6c12dac67350
Duc Ho <hotienduc05012002@gmail.com>
2025-02-22 01:03:52 -0700

    Added producing item into player inventory

--- scripts/gui/temp_placing_mode.gd
+++ scripts/gui/temp_placing_mode.gd
@@ -9,8 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if GameManager.is_placing_gadget:
-		text = "Gadget"
+		text = "Placing Mode: Wheel"
 	else:
-		text = "None"
+		text = "Placing Mode: None"
 	
 	
--- scripts/objects/gadget.gd
+++ scripts/objects/gadget.gd
@@ -2,14 +2,28 @@ extends StaticBody2D
 
 @onready var gadget_stats = load("res://resources/gadgets/wheel.tres")
 
+@export var output_inventory: Inventory
+
+var character: Node2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	character = get_parent().get_parent().get_parent().find_child("Character")
 	$Sprite2D.texture = gadget_stats.texture
 	$Timer.wait_time = gadget_stats.process_time
 	$Timer.start()
+	$Timer.timeout.connect(add_item_to_inventory)
 	$TextureProgressBar.visible = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$TextureProgressBar.value = 100 - ($Timer.time_left / $Timer.wait_time) * 100
+	
+func add_item_to_inventory() -> void:
+	var item: Item = load("res://resources/items/cotton.tres")
+	collect(item)
+		
+## 'Collects' a given item, placing it into the inventory on the nearest open slot
+func collect(item: Item):
+	character.inventory.insert(item)
