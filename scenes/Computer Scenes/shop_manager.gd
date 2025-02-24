367d9b027e8ed4d762b1233defea568d81726f1d
Zhasheken de Guia <97582439+venetiaz@users.noreply.github.com>
2025-02-24 15:20:34 -0700

    FEAT: bought items added to inventory

--- scenes/Computer Scenes/shop_manager.gd
+++ scenes/Computer Scenes/shop_manager.gd
@@ -11,21 +11,11 @@ var categorized_items: Dictionary = {
 	"items": []
 }
 
+# **** ITEMS ARE BOUGHT IN item_buy_popup.gd ****
 func _ready():
 	load_shop_items()
 	create_shop_categories()
-	#create_inbox_buttons()
-	#print(emails)
-	#display_category_emails("main") # default view to "main"
 	
-func add_item():
-	pass
-	#var item = item_class.new()  
-	#items.append(item) 
-	#display_item(item)  # Add to UI
-func add_gadget():
-	pass
-# adds items to shop ui
 func display_item(item: Item): #insert item as a parameter
 	var item_button = item_button_scene.instantiate()  
 	item_button.set_item(item)
@@ -43,7 +33,7 @@ func load_shop_items():
 			if file_name.ends_with(".tres"):
 				var item_path = item_folder + file_name
 				var item = load(item_path)
-				if item and item is Item:
+				if item and item is Item: # TODO: change in future for gadget upgrades, etc
 					categorized_items["items"].append(item)
 			file_name = dir.get_next()
 		dir.list_dir_end()
@@ -54,17 +44,4 @@ func create_shop_categories():
 		#inbox_button.text = category.capitalize()
 		#inbox_button.pressed.connect(func(): display_category_emails(category)) 
 		categories_list_container.add_child(shop_category)
-# shows upon click
-
-func buy_item(item: Item, qty: int):
-	pass
-	#TODO: confirm sufficient currency
-	#TODO: confirm sufficient inventory space
-	#TODO: add item to inventory, subtract currency
-
-func sell_item(item: Item, qty: int):
-	pass
-	#TODO: confirm possession of items for selling
-	#TODO: remove item from inventory, add currency
-#func show_item_details(item: Item, item_button: Button): #confirm item buy???
 	
