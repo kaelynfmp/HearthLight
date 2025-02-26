1cb4d932516ddcf2fbb78801a0ca021a793c9617
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-02-26 16:34:51 -0700

    feat: dynamic inventory ui

---
+++ scenes/inventory/slots.gd
@@ -0,0 +1,20 @@
+extends HFlowContainer
+
+var inventory = load("res://resources/character/inventory.tres")
+var prev_slots:int = 0
+
+func add_slots():
+	for child in get_children():
+		child.free()
+		
+	for slot in inventory.slots:
+		var new_scene:PanelContainer = load("res://scenes/inventory/slot.tscn").instantiate()
+		new_scene.slot = slot
+		var bg_image:TextureRect = load("res://scenes/inventory/inventory_slot_image.tscn").instantiate()
+		new_scene.add_child(bg_image)
+		add_child(new_scene)
+
+func _process(_delta: float) -> void:
+	if prev_slots != inventory.slots.size():
+		prev_slots = inventory.slots.size()
+		add_slots()
