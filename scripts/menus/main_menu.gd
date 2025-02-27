d4df96879d29f60068227697316f218aa4ace0c0
Duc Ho <hotienduc05012002@gmail.com>
2025-02-27 01:29:42 -0700

    Added main menu

---
+++ scripts/menus/main_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/room.tscn")
+
+
+func _on_credit_pressed() -> void:
+	$Label.visible = false
+	$MainMenuButtonContainers.visible = false
+	$CreditContainers.visible = true
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	$Label.visible = true
+	$MainMenuButtonContainers.visible = true
+	$CreditContainers.visible = false # Replace with function body.
