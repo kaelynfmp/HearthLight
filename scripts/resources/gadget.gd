d5202ad06623adfa18b868cfdbeb94c26656826f
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-02-27 11:48:18 -0700

    fix: fix gadget scene and make gadgets automatically lock the correct slots

--- scripts/resources/gadget.gd
+++ scripts/resources/gadget.gd
@@ -45,6 +45,10 @@ var prev_item_texture
 			var temp_inventory_slots:Array[Slot] = inventory.slots.duplicate()
 			temp_inventory_slots.resize(inputs + outputs)
 			inventory.slots = temp_inventory_slots
+			for index in range(inventory.slots.size()):
+				var slot = inventory.slots[index]
+				if index >= inputs:
+					slot.locked = true
 		return max(1, inputs)
 ## The amount of outputs this gadget can produce
 @export var outputs: int:
