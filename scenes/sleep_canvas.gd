082a41225a199000f972dd2e1d8d5ab76db644bf
Zhasheken de Guia <97582439+venetiaz@users.noreply.github.com>
2025-03-30 06:31:24 -0600

    Update sleep_canvas.gd

--- scenes/sleep_canvas.gd
+++ scenes/sleep_canvas.gd
@@ -1,15 +1,16 @@
 extends Control
-@onready var timer = Timer.new()
 func _ready():
 	visible = true
 	modulate.a = 0
-	add_child(timer)
-	timer.wait_time = 2.0
-	timer.one_shot = true
 func _process(delta: float) -> void:
 	if GameManager.sleeping:
 		visible = true
-		modulate.a = 0.2
+		if modulate.a < 0.7:
+			modulate.a += 0.005
+		if modulate.a >= 0.7:
+			pass
 	else:
 		visible = false
 		modulate.a = 0
+	
+	
