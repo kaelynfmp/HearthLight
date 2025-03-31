95b152bb104f95c07afb86a7c1316667e70564fd
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-03-30 19:09:00 -0600

    feat: clock work with tutorial

--- scenes/clock.gd
+++ scenes/clock.gd
@@ -3,14 +3,10 @@ var game_time = GameManager.game_time
 @export var day_label: RichTextLabel 
 @export var time_label: RichTextLabel
 var initial_position
-var day_initial
-var time_initial
 
 func _ready():
 	set_process(true)
 	initial_position = position
-	day_initial = day_label.position
-	time_initial = time_label.position
 
 func _process(_delta: float) -> void:
 	day_label.text = "Day "+ str(game_time["day"])
@@ -20,12 +16,11 @@ func _process(_delta: float) -> void:
 		time_label.text = str(game_time["hour"])+":"+str(game_time["minute"])
 	
 	if GameManager.in_computer:
-		anchor_left = 0.5
-		anchor_right = 0.5
-		set_anchors_preset(Control.PRESET_CENTER_TOP)
-		position.x = 900  # Moves it left by half its width
+		day_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		time_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		position.y = 0
 		
 	else:
-		position = initial_position  
-		day_label.set_position(day_initial)
-		time_label.set_position(time_initial)
+		position = initial_position
+		day_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
+		time_label.horizontal_alignment = HORIZONTAL_ALIGNMENT_LEFT
