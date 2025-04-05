2f30c410d4017ba8afc3109d1807541f0133329e
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2025-04-04 23:38:04 -0600

    feat: notifications in emails

--- scenes/Computer Scenes/inbox_buttons.gd
+++ scenes/Computer Scenes/inbox_buttons.gd
@@ -2,9 +2,11 @@ extends Button
 
 @export var button_sound:AudioManager.BUTTON
 @export var sprite:Sprite2D
+@export var notification:Panel
 var main_texture:Texture
 var category:String
 var prev_empty:bool = false
+var prev_has_unreads:bool = false
 
 func _ready() -> void:
 	sprite.texture = main_texture
@@ -19,3 +21,11 @@ func _physics_process(_delta: float) -> void:
 		prev_empty = false
 		disabled = false
 		sprite.modulate = Color(1, 1, 1, 1)
+	if not disabled:
+		var has_unreads = GameManager.categorized_emails[category].any(func(email: Email): return email.check_valid() and not email.is_read)
+		if prev_has_unreads != has_unreads:
+			prev_has_unreads = has_unreads
+			if has_unreads:
+				notification.set_visible(true)
+			else:
+				notification.set_visible(false)
